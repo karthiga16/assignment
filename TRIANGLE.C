b1bafae1ac16ed16937acb1c047cdb40c40dc931
karthiga <karthidhanu7@gmail.com>
2020-10-09 10:08:05 -0700

    triangle.c

---
+++ TRIANGLE.C
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+int i, j, n;
+clrscr();// the number of rows of floyd's triangle to print\n");
+//anf("%d", &n);
+for(i=1; i<=5; i++)
+{
+for(j=1; j<=i; j++);
+{
+printf("* ");
+}
+printf("\n");
+}
+return 0;
+}
